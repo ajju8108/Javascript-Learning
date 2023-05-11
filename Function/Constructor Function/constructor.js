61a17a1f83ccb343e9ca03b46eafadce5b291256
ajju8108 <ajaymaurya199618@gmail.com>
2023-05-11 11:59:33 +0530

    Dynamic Object Property

--- Function/Constructor Function/constructor.js
+++ Function/Constructor Function/constructor.js
@@ -11,6 +11,10 @@
 //     };
 // }
 
+
+// This references an empty obj 
+// When we use new operator 3 things happen first it will an empty obj then it will set "this" to point at the empty object and finally it will return the obj from the constructer function 
+
 // Constructor Function  
 function MyCircle(radius) {
     this.radius = radius
