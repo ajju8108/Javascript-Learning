b415a65b6c0672aef289d82ef7bf314a5e97a0a3
ajju8108 <ajaymaurya199618@gmail.com>
2023-05-11 18:22:02 +0530

    spread operator

---
+++ Arrays/Spread Operator/spread.js
@@ -0,0 +1,7 @@
+const first = [1, 2, 3]
+const second = [4, 5, 6]
+
+// const combined = [...first, ...second]
+// we can add elemet anywhere ex
+const combined = [...first, 9, ...second, 33]
+console.log(combined);
